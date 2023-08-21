078fca7f60df807b25f03a732ffeb61ead055d57
Samy CHBINOU <120525869+chbinousamy@users.noreply.github.com>
2023-08-21 15:34:07 +0200

    Update render.js

--- render.js
+++ render.js
@@ -5,7 +5,7 @@ var path = require('path');
 var theme = require('jsonresume-theme-eloquent');
 
 var resumeJSON = JSON.parse(fs.readFileSync(path.join(__dirname, '/resume.json'), 'utf-8'));
-var resumeHTML = eloquent.render(resumeJSON);
+var resumeHTML = theme.render(resumeJSON);
 
 fs.writeFile(path.join(__dirname, '/docs/resume.html'), resumeHTML, function(err) {
     console.log(err || 'Resume exported to resume.html');
